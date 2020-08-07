63ebba276f958a5faafa75e198d29448bbe96171
= <arthurreis074@gmail.com>
2020-08-07 19:39:48 -0300

    Updates

---
+++ precache-manifest.78a688356a7eaae3601b830e665562f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58eff6fba55e1c38fef2081380a69c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af3793c1822b9e4577ea",
+    "url": "/static/js/2.e8644690.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e8644690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e06daf28d6e58af5b143",
+    "url": "/static/js/main.57a3980a.chunk.js"
+  },
+  {
+    "revision": "23a8542c495de0a29ff2",
+    "url": "/static/js/runtime-main.f51e40a7.js"
+  },
+  {
+    "revision": "877fb2eee3a26f253ba6271dd4ef4324",
+    "url": "/static/media/back.877fb2ee.svg"
+  },
+  {
+    "revision": "77757c08eda83cc144b897731a874fd3",
+    "url": "/static/media/background.77757c08.svg"
+  },
+  {
+    "revision": "9b6f965d166f3bfd8d9f54a3a8a68aa2",
+    "url": "/static/media/go.9b6f965d.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/logo.04f6798f.svg"
+  }
+]);
