15b3f16adcd148289ec8dd134c30f3b90fd0bbb2
= <arthurreis074@gmail.com>
2020-08-06 22:16:19 -0300

    Initial Commit

---
+++ src/Routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+// => Components
+import Dashboard from './pages/Dashboard'
+import Repository from './pages/Repository'
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Dashboard} />
+        <Route path="/repository" component={Repository} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
